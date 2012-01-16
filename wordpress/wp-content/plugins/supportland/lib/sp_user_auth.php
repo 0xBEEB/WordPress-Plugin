3903253f46e3254347b3dff9a0584df0f4aff1ca
Alexis Carlough <alexiscarlough@gmail.com>
2012-01-15 20:01:15 -0800

    Login outputs cookie info as well

--- wordpress/wp-content/plugins/supportland/lib/sp_user_auth.php
+++ wordpress/wp-content/plugins/supportland/lib/sp_user_auth.php
@@ -9,7 +9,8 @@
     try
     {
         $user->authenticate($login_email, $login_password);
-        echo "Your access token: " . $user->get_access_token();
+        echo "Your access token: " , $user->get_access_token() , "\n";
+        echo "Cookie info: " . $_COOKIE['sp_access_token'];
     }catch(Exception $e)
     {
         echo 'Caught exception: ' , $e->getMessage();
