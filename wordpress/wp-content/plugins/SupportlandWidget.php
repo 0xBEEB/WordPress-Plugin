c4ad1eba9d47a4404ceea9787102ac632334c7ea
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2012-01-18 20:53:26 -0800

    widget has jquery slide effect links

--- wordpress/wp-content/plugins/SupportlandWidget.php
+++ wordpress/wp-content/plugins/SupportlandWidget.php
@@ -19,10 +19,68 @@ License: GPLv2.0 compatible (TBA)
 
 function headerStuff() {
 echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>';
+echo '<style type="text/css">
+	.spMenuLink{cursor:pointer;}
+	#spResult1,#spResult2,#spResult3,#spResult4{display:none;}
+	.spPlusMinusWrap{display:inline-block;height:15px;width:16px;padding:0;margin:0 6px 0 0;}
+	.spMinus {display:inline-block;background:url(http://www.pasadenaplayhouse.org/images/minus_icon.png) top left no-repeat;height:15px;width:16px;}
+	div,table{border-collapse:collapse;}
+	a img{border:none;}
+</style>';
 }
 
 function supportland() {
-echo "hellooooo!!!1";
+echo '<div id="spMenuLink1" class="spMenuLink"><span class="spPlusMinusWrap"><span class="spMinus"><img src="http://www.pasadenaplayhouse.org/images/plus_icon.png" id="spPlus1" /></span></span><a>Card</a></div>
+<div id="spResult1">
+  Some card content goes here lorem ipsum blah blah blah
+</div>';
+echo '<div id="spMenuLink2" class="spMenuLink"><span class="spPlusMinusWrap"><span class="spMinus"><img src="http://www.pasadenaplayhouse.org/images/plus_icon.png" id="spPlus2" /></span></span><a>Wallet</a></div>
+<div id="spResult2">
+  Some wallet content goes here lorem ipsum blah blah blah
+</div>';
+echo '<div id="spMenuLink3" class="spMenuLink"><span class="spPlusMinusWrap"><span class="spMinus"><img src="http://www.pasadenaplayhouse.org/images/plus_icon.png" id="spPlus3" /></span></span><a>Businesses</a></div>
+<div id="spResult3">
+  Some businesses content goes here lorem ipsum blah blah blah
+</div>';
+echo '<div id="spMenuLink4" class="spMenuLink"><span class="spPlusMinusWrap"><span class="spMinus"><img src="http://www.pasadenaplayhouse.org/images/plus_icon.png" id="spPlus4" /></span></span><a>Search</a></div>
+<div id="spResult4">
+  Some search content goes here lorem ipsum blah blah blah
+</div>';
+
+
+echo "<script>
+$('#spMenuLink1').click(function() {
+  $('#spResult1').slideToggle('fast', function() {
+    // Animation complete.
+    $('#spPlus1').toggle();
+  });
+});
+</script>";
+echo "<script>
+$('#spMenuLink2').click(function() {
+  $('#spResult2').slideToggle('fast', function() {
+    // Animation complete.
+    $('#spPlus2').toggle();
+  });
+});
+</script>";
+echo "<script>
+$('#spMenuLink3').click(function() {
+  $('#spResult3').slideToggle('fast', function() {
+    // Animation complete.
+    $('#spPlus3').toggle();
+  });
+});
+</script>";
+echo "<script>
+$('#spMenuLink4').click(function() {
+  $('#spResult4').slideToggle('fast', function() {
+    // Animation complete.
+    $('#spPlus4').toggle();
+  });
+});
+</script>";
+
 }
 
 function init_supportland(){
