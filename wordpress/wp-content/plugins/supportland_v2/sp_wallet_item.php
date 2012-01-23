e9cfb1b9ebb99915c7c9f5ef178cee5c2df56467
Casey Beach <mcsmash@capstoneaa.cs.pdx.edu>
2012-01-22 18:17:45 -0800

    working login wallet etc

--- wordpress/wp-content/plugins/supportland_v2/sp_wallet_item.php
+++ wordpress/wp-content/plugins/supportland_v2/sp_wallet_item.php
@@ -1,14 +1,11 @@
 <?php
 	// This method will return $wallet item.
-	require 'sp_api.php';
-	require 'sp_login.php';
-	
-	
+	require_once 'sp_api.php';
+	require_once 'sp_login.php';
 	
 	function sp_wallet_item() {
 		$sp_user = new SP_User();
 		$sp_trans = new SP_Transaction($sp_user);
-		
 		try {
 			$wallet = $sp_trans->get_wallet();
 			$wallet = json_decode($wallet);
@@ -20,6 +17,5 @@
 			return;
 		}
 	}
-	
 
 ?>
