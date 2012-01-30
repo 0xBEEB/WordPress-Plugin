8c75c1acf23fd041f85ae8d77467bb660289a70d
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2012-01-30 14:45:31 -0800

    copied 0.2 from main branch to lochlan-dev

--- wordpress/wp-content/plugins/supportland/supportland.php
+++ wordpress/wp-content/plugins/supportland/supportland.php
@@ -37,18 +37,16 @@ add_action( 'wp_head', sp_headerStuff);
 include 'supportland-settings.php';
 
 //Output Google-hosted jQuery and some CSS (used with jQuery) into the <head> tag
-function sp_headerStuff() {?>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-	<style type="text/css">
-		.spMenuLink{padding:2px 3px 3px 3px;cursor:pointer;line-height:1.5em;}
-		.spMenuLink:hover{background-color:#eee;}
-		#spResult1,#spResult2,#spResult3,#spResult4{display:none;}
-		.sp_plusMinusCircle{bottom:-3px;background-color:#a21;height:16px;width:16px;border-radius: 8px;-moz-border-radius:8px;position:relative;display:inline-block;}
-		.sp_plusMinusHBar{background-color:#fff;height:2px;width:8px;position:absolute;top:7px;left:4px;}
-		.sp_plusMinusVBar{background-color:#fff;height:8px;width:2px;position:absolute;top:4px;left:7px;}
-		.sp_Result{margin-left:11px;padding-left:11px;border-left:1px dashed #ccc;} 
-	</style>
-<?}
+function sp_headerStuff() {
+echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>';
+echo '<style type="text/css">
+	.spMenuLink{cursor:pointer;}
+	#spResult1,#spResult2,#spResult3,#spResult4{display:none;}
+	.spPlusMinusWrap{display:inline-block;height:15px;width:16px;padding:0;margin:0 6px 0 0;}
+	.spMinus {display:inline-block;background:url(http://www.pasadenaplayhouse.org/images/minus_icon.png) top left no-repeat;height:15px;width:16px;}
+	.spPlus {display:inline-block;background:url(http://www.pasadenaplayhouse.org/images/plus_icon.png) top left no-repeat;height:15px;width:16px;}
+</style>';
+}
 
 function init_supportland(){
   register_sidebar_widget('Supportland', 'supportland');
@@ -60,41 +58,50 @@ function supportland() {
 	$app_token = $plugin_options['app_token_text_string'];
 	
 	//The content for the four sections will at some point come from queries to the API, but for now is hard coded as these dummy strings.
-	$spCard = "Card info goes here.  The app token is ".$app_token; //stuffed the app token in here just to show that it works
+	$spCard = "Card info goes here.  The app token is ".$app_token; //stuffed the app key in here just to show that it works
 	//Casey: store all the wallet stuff in a string called $spWallet and delete the following line
 	$spWallet = "Rewards<br />Points Earned<br />Punch Cards<br />Coupons<br />Supportland Card";
 	$spBusiness = "Business section.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quam ante, mattis interdum semper non, bibendum quis metus. Vestibulum sem risus, eleifend ac adipiscing nec, hendrerit a sem. Curabitur nec augue id lectus feugiat posuere. Phasellus in magna ante, non sagittis ligula.";
 	$spSearch = "Search content goes here.  There will be fields for querying the Supportland search API.";
-
+	
 ?>
 	<div id="spMenuLink1" class="spMenuLink">
-		<span class="sp_plusMinusCircle"><span class="sp_plusMinusHBar"></span><span class="sp_plusMinusVBar" id="spPlus1"></span></span>
-		<a>Card</a>
+		<span class="spPlusMinusWrap">
+			<span class="spMinus">
+				<span class="spPlus" id="spPlus1"></span>
+			</span>
+		</span><a>Card</a>
 	</div>
-	<div class="sp_Result" id="spResult1"><? echo $spCard; ?>
+	<div id="spResult1"><? echo $spCard; ?>
 	<br /></div>
 
 	<div id="spMenuLink2" class="spMenuLink">
-		<span class="sp_plusMinusCircle"><span class="sp_plusMinusHBar"></span><span class="sp_plusMinusVBar" id="spPlus2"></span></span>
-		<a>Wallet</a>
+		<span class="spPlusMinusWrap">
+			<span class="spMinus">
+				<span class="spPlus" id="spPlus2"></span>
+			</span>
+		</span><a>Wallet</a>
 	</div>
-	<div class="sp_Result" id="spResult2"><? echo $spWallet; ?></div>
+	<div id="spResult2"><? echo $spWallet; ?></div>
 
 	<div id="spMenuLink3" class="spMenuLink">
-		<span class="sp_plusMinusCircle"><span class="sp_plusMinusHBar"></span><span class="sp_plusMinusVBar" id="spPlus3"></span></span>
-		<a>Business</a>
+		<span class="spPlusMinusWrap">
+			<span class="spMinus">
+				<span class="spPlus" id="spPlus3"></span>
+			</span>
+		</span><a>Business</a>
 	</div>
-	<div class="sp_Result" id="spResult3"><? echo $spBusiness; ?></div>
+	<div id="spResult3"><? echo $spBusiness; ?></div>
 	
 	<div id="spMenuLink4" class="spMenuLink">
-		<span class="sp_plusMinusCircle"><span class="sp_plusMinusHBar"></span><span class="sp_plusMinusVBar" id="spPlus4"></span></span>
-		<a>Search</a>
+		<span class="spPlusMinusWrap">
+			<span class="spMinus">
+				<span class="spPlus" id="spPlus4"></span>
+			</span>
+		</span><a>Search</a>
 	</div>
-	<div class="sp_Result" id="spResult4"><? echo $spSearch; ?></div>
+	<div id="spResult4"><? echo $spSearch; ?></div>
 
-	
-	
-	
 <?	//jQuery animations for the four sections
 	for($i=1;$i<=4;$i++) { ?>
 		<script>
