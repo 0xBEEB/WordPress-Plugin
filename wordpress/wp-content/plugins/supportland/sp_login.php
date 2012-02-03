35b1cfc25b5415860fb6c996e94ed7b0880ee66f
Lochlan McIntosh <lochlan.mcintosh@gmail.com>
2012-02-03 14:02:05 -0800

    small <?php fix

--- wordpress/wp-content/plugins/supportland/sp_login.php
+++ wordpress/wp-content/plugins/supportland/sp_login.php
@@ -11,7 +11,7 @@
             <?php echo sp_search_local_store(); ?>
             <a class="login" style="margin:5px;cursor:pointer;font-size:12px;font-weight:bold;">Login</a><br />
             <div class="hide_login_form" style="display:none;font-size:12px;font-weight:normal;">
-        	 		<?= sp_login_form() ?>
+        	 <?php echo sp_login_form(); ?>
             </div>
             <div style="margin:5px" id="forgot_pass"><a>Forgot your password?</a></div>
             <a class="sp_signup" style="margin:5px;cursor:pointer;font-size:12px;font-weight:bold"> Sign up!</a><br />
@@ -32,12 +32,12 @@
                 $('.hide_login_form').toggle('fast',function() {})
             });	
         </script>
-<?    }
+<?php }
 
     function sp_login_form() {
     	if (isset($_GET['sp_bad_auth']) && $_GET['sp_bad_auth'] == 1) {?>
             <p><font color=red>Bad email or password</font></p>
-<?      } ?>
+<?php   } ?>
         <form action="wp-content/plugins/supportland/lib/sp_user_auth.php">
             <input type="hidden" name="sp_loc" value="Location:<?= home_url()?>" />
             <label style="margin:5px;" for="login_email">Email</label></br>
@@ -46,12 +46,12 @@
             <input style="margin:5px;" type="password" name="sp_login_password" id="login_password"/> <br />
             <p style= "margin:5px;" align="right"> <input name="login" type="submit" value="Log in" /> </p>
         </form>
-<?  }
+<?php }
 
     function sp_search_local_store() { ?>
         <label style="margin:5px;" for="sp_search_store">Search local store</label> </br>
         <input style="margin:5px;" type="search" name="sp_search_store" id="sp_search_store" /> <br />
         <p style="margin:5px;" align="right"> <input name="sp_search_submit" type="submit" value="Search" /> </p>
-<?  }
+<?php }
 
 ?>
