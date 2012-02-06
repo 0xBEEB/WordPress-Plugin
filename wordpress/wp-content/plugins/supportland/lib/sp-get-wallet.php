0fab68eb0a11b2c7b9cdac356ea1f27796d5374c
Alexis Carlough <alexiscarlough@gmail.com>
2012-02-06 08:44:22 -0800

    Changed file names to follow WP coding standards

---
+++ wordpress/wp-content/plugins/supportland/lib/sp-get-wallet.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'sp-api.php';
+ 
+    $my_user = new SP_User();
+    $my_trans = new SP_Transaction($sp_user);
+    $sp_loc = null;
+
+    if (isset($_GET['sp_loc']))
+        $sp_loc = $_GET['sp_loc'];
+
+    if($my_user->logged_in() == true)
+        die( $my_trans->get_wallet());
+    else
+        header($sp_loc);
+
+
+
+
+
+?>
