af112105755e59a567d599db69fe0bf84d4ee698
David Liang <davidliang2008@hotmail.com>
2012-02-07 17:52:43 -0800

    simplified javascript folder

--- wordpress/wp-content/plugins/supportland/js/sp.js
+++ wordpress/wp-content/plugins/supportland/js/sp.js
@@ -1,35 +1,37 @@
 jQuery(document).ready(function($){
-    // login form submit
-    $("#sp_submit_login").click(function(){
+    // login form validation
+    $("#sp_login_btn").click(function(){
         var email = $("#sp_login_email").val();
-        var password = $("#sp_login_password").val();
-        $.ajax({
-            url: 'wp-content/plugins/supportland/lib/sp_user_auth.php',
-            dataType: 'json',
-            type: 'POST',
-            data: {sp_login_email: email, sp_login_password: password},
-            success: function(res) {
-                $("#sp_login_form").hide();
-                $("#sp_user_info").show();
-                $("#sp_user_name").text(res.public_name);
-                $("#sp_user_id").text(res.id);
-                var d = new Date(res.member_since);
-                $("#sp_user_register_date").text(d.toDateString());
-                $("#sp_user_points").text(res.points);
-            },
-            error: function() {
-                clear_user_infor();
-                $("#sp_login_form").show();
-                $("#sp_user_info").hide();
-            }
-        });
+        if (email == '' || !is_valid_email(email)) {
+            // show email error message
+            $("#sp_login_email_error").show();
+            $("#sp_login_email").css('border-color', 'orange red');
+            return false;
+        }
+        else {
+            $("#sp_login_email_error").hide();
+            $("#sp_login_email").css('border-color', '#DDDDDD');
+        }
+        var pw = $("#sp_login_password").val();
+        if (pw == '') {
+            // show password error message
+            $("#sp_login_pw_error").show();
+            $("#sp_login_password").css('border-color', 'orange red');
+            return false;
+        }
+        else {
+            $("#sp_login_pw_error").hide();
+            $("#sp_login_password").css('border-color', '#DDDDDD');
+        }
+        $(this).submit();
     });
     
-    function clear_user_infor() {
-        $("#sp_user_name").text('');
-        $("#sp_user_id").text('');
-        $("#sp_user_register_date").text('');
-        $("#sp_user_points").text('');
+    function is_valid_email(email) {
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        if (!emailReg.test(email)) 
+            return false;
+        else
+            return true;
     }
 });
 
