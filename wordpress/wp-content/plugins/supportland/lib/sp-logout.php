5b6cb960ee51b842ffa86a8dcc927382a89a9c45
David Liang <davidliang2008@hotmail.com>
2012-02-08 19:10:46 -0800

    modified sp-logout file

--- wordpress/wp-content/plugins/supportland/lib/sp-logout.php
+++ wordpress/wp-content/plugins/supportland/lib/sp-logout.php
@@ -3,11 +3,10 @@
     add_action('init', 'sp_set_cookies');
     add_action('init', 'sp_unset_cookies');
     $sp_user = new SP_User();
-    $sp_loc = null;
-
+    //$sp_loc = null;
+    /*
     if (isset($_GET['sp_loc']))
-        $sp_loc = $_GET['sp_loc'];
-
+        $sp_loc = $_GET['sp_loc']; */
     $sp_user->logout(); 
-    header($sp_loc);
+    //header($sp_loc);
 ?>
