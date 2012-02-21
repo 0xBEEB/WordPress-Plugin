6736bcd050d6c10764538ed4b9f5ea8430042bf4
David Liang <davidliang2008@hotmail.com>
2012-02-20 16:26:29 -0800

    used _POST instead of _GET

--- supportland/lib/sp-user-auth.php
+++ supportland/lib/sp-user-auth.php
@@ -3,18 +3,14 @@
     add_action('init', 'sp_set_cookies');
     add_action('init', 'sp_unset_cookies');
 
-    //$sp_login_email = $_POST["sp_login_email"];
-    //$sp_login_password = $_POST["sp_login_password"];
-    $sp_login_email = $_GET["sp_login_email"];
-    $sp_login_password = $_GET["sp_login_password"];
+    $sp_login_email = $_POST["sp_login_email"];
+    $sp_login_password = $_POST["sp_login_password"];
 
     $user = new SP_User();
     try
     {
         $user->authenticate($sp_login_email, $sp_login_password);
-        //$user->fetch_user_info();
-        //echo $user->user_info;
-        exit("Yes");
+        die();
     }catch(Exception $e)
     {
         die($e->getMessage());
