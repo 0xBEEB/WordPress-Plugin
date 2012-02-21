2480ed8fafbf1010c109996e0bc2a27a0ff0d0ec
David Liang <davidliang2008@hotmail.com>
2012-02-20 16:21:04 -0800

    supportland widget class that extends WordPress built-in widget class

---
+++ supportland/class-sp-widget.php
@@ -0,0 +1,126 @@
+<?php
+/*
+ * Supportland widget class, extend WordPress built-in widget class WP_Widget
+ */
+require_once 'lib/sp-api.php';
+
+class SP_Widget extends WP_widget {
+    // Constructor
+    function SP_Widget() {
+        $widget_options = array(
+            'classname' => 'supportland-widget',
+            // Description that appears the widget plugin before being dragged
+            //  to widget areas
+            'description' => 'Supportland Wallet Widget, [xxxx]'
+        );
+        parent::WP_Widget(
+                'supportland_widget',
+                'Supportland Widget',
+                $widget_options);
+    }
+    
+    // Output the user interface of the widget
+    //      $args - arguments from the theme
+    //      $instance - the instance of the class
+    function widget($args, $instance) {
+        // if app token is not present, show error
+        if ($instance['show_error']) {
+            ?>
+                <div style="color:red;">
+                    Supportland app token not set. <br /><br />
+                    Go to Supportland Widget (Appearance->Widgets) to enter
+                    app token.
+                </div>
+            <?php
+        }
+        else {
+            $sp_user = new SP_User();
+            // if the user is logged in, display the main menu
+            if ($sp_user->logged_in()) {
+                $this->display_main_menu();
+            }
+            // else, display the login page
+            else {
+                $this->display_login_page();
+            }
+        }
+    }
+    
+    // Update callback when pressing 'save' button on the widget
+    function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        // strip tags to remove HTML
+        $sp_app_token = strip_tags($new_instance['sp_app_token']);
+        // if sp_app_token is not set or empty, set 'show_error' to be true
+        if (!$sp_app_token || $sp_app_token == '') {
+            $instance['sp_app_token'] = '';
+            $instance['show_error'] = true;
+        }
+        // else, set 'sp_app_token' in instance object
+        else {
+            $instance['sp_app_token'] = $sp_app_token;
+            $instance['show_error'] = false;
+            // save app token to option table in the database
+            $this->save_app_token($sp_app_token);
+        }
+        return $instance;
+    }
+    
+    // Handle configuration of the widget after being dragged to widget area
+    function form($instance) {
+        // set up default settings of the instance, include field 'show_error'
+        // and field 'sp_app_token'
+        $default = array (
+            'sp_app_token' => '',
+            'show_error' => true
+        );
+        // display the Lochlan's setting page
+        ?>
+            <div>
+                <p>
+                    To obtain an App Token, please email 
+                    <a href="mailto:help@supportland.com">help@supportland.com</a>
+                    with the subject "App Token Request (your name)" and we will
+                    respond as soon as  we can.
+                </p>
+                <p>
+                    For more information, visit
+                    <a href="http://www.supportland.com">supportland.com</a>
+                </p>
+                <label for="<?php echo $this->get_field_id('sp_app_token'); ?>">
+                    App Token</label>
+                <input id="<?php echo $this->get_field_id('sp_app_token'); ?>"
+                       name="<?php echo $this->get_field_name('sp_app_token')?>"
+                       type="text"
+                       value="<?php echo esc_attr($instance['sp_app_token']); ?>"
+                       style="width: 100%;" />
+                <div style="color:red;
+                     display: <?php if($instance['show_error']) echo 'block';
+                     else echo 'none'; ?>;">App token string is required.</div>
+            </div>
+        <?php
+    }
+    
+    function display_main_menu() {
+        require_once 'sp-mainMenu.php';
+        sp_mainMenu();
+    }
+    
+    function display_login_page() {
+        require_once 'sp-login.php';
+        sp_login_page();
+    }
+    
+    function save_app_token($token) {
+        $sp_app_token = get_option('sp_app_token');
+        // if not exist, insert
+        if (!sp_app_token) {
+            add_option('sp_app_token', $token);
+        }
+        // otherwise, update
+        else {
+            update_option('sp_app_token', $token);
+        }
+    }
+}
+?>
