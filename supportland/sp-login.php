0bec291d5f2cd42195618cb18a8a9fb4eca69fd7
David Liang <davidliang2008@hotmail.com>
2012-03-05 23:17:15 -0800

    redesign login interface; currently comment out search area

--- supportland/sp-login.php
+++ supportland/sp-login.php
@@ -2,18 +2,12 @@
 function sp_login_page() {
     ?>
         <div id="sp_login_wrapper">
-            <div id="sp_login_top">
-                <a href="">Welcome to Supportland!</a>
-            </div>
-            <hr />
             <!-- display search store form -->
-            <?php display_search(); ?>
+            <?php //display_search(); ?>
             <!-- display login form -->
             <?php display_login_form(); ?>
-            <!-- display forgot password area -->
-            <?php display_forgot_pw(); ?>
-            <!-- display sign up area -->
-            <?php display_sign_up(); ?>
+            <!-- display other links like forgot password, register, etc -->
+            <?php display_login_otherlinks(); ?>
         </div>
     <?php
 }
@@ -30,77 +24,56 @@ function display_search() {
 
 function display_login_form() {
     ?>
-        <div id="sp_login_area">
-            <a>Login</a><br />
-            <div id="sp_login_form_content" style="display:none;">
-                <!-- display invalid login error message -->
-                <div id="sp_login_error" style="display: none;"></div>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <label for="sp_login_email">Email</label>
-                            </td>
-                            <td>
-                                <!-- display invalid email error message -->
-                                <div id="sp_login_email_error" style="display:none;">
-                                    *** invalid email
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <input type="text" id="sp_login_email" name="sp_login_email" value="" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="sp_login_password">Password</label>
-                            </td>
-                            <td>
-                                <!-- display empty password error message -->
-                                <div id="sp_login_pw_error" style="display:none;">
-                                    *** can't be empty
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <input type="password" id="sp_login_password" name="sp_login_password" value="" />
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div>
-                    <span>
-                        <img id="sp_login_loader" src="wp-content/plugins/supportland/images/ajax-loader.gif" alt="loading..." />
-                    </span>&nbsp;&nbsp;
-                    <input type="button" id="sp_login_btn" name="sp_login_btn" value="Log in" />
-                </div>
+        <label class="title_font">Log in</label>
+        <div id="sp_login_form">
+            <!-- display invalid login error message -->
+            <div id="sp_login_error" class="sp_login_error" style="display: none;"></div>
+            <table>
+                <tr>
+                    <td><label for="sp_login_email">Email</label></td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" id="sp_login_email" name="sp_login_email" value="" />
+                        <span id="sp_login_email_error" class="sp_error" style="display:none;">must be a full email address</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="sp_login_password">Password</label></td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="password" id="sp_login_password" name="sp_login_password" value="" />
+                        <span id="sp_login_pw_error" class="sp_error" style="display:none;">password can not be empty</span>
+                    </td>
+                </tr>
+            </table>
+            <div>
+                <span>
+                    <img id="sp_login_loader" src="wp-content/plugins/supportland/images/ajax-loader.gif" alt="loading..." style="display:none;" />
+                </span>&nbsp;&nbsp;
+                <input type="button" id="sp_login_btn" class="sp_btn" name="sp_login_btn" value="Log in" />
             </div>
         </div>
     <?php
 }
 
-function display_forgot_pw() {
-    ?>
-        <div id="forgot_pass">
-            <a href="http://supportland.com/">Forgot your password?</a>
-        </div>
-    <?php
-}
-
-function display_sign_up() {
+function display_login_otherlinks() {
     require_once 'sp-signup-form.php';
     ?>
-        <div id="sp_signup">
-            <a id="sp_signup_a" href="#signupform">Sign up!</a>
-            <div style="display:none;">
-                <div id="signupform">
-                    <?php sp_signup_form(); ?>
-                </div>
-            </div>
+        <div id="sp_login_otherlinks">
+            <ul>
+                <li>
+                    <a href="http://supportland.com/">Forgot your password?</a>
+                </li>
+                <li>
+                    <a id="sp_register_anchor" href="#sp_registration_form">Register</a>
+                    <div style="display: none;">
+                        <?php echo sp_signup_form(); ?>
+                    </div>
+                </li>
+            </ul>
         </div>
-    <?php
+    <?php    
 }
 ?>
